f3c06d86ac5bec0d95a14d17ddf943e8fa08f6de
mlgomez0 <mlgomez230@hotmail.com>
2020-07-14 14:15:02 +0000

    fix output and clean

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -4,12 +4,13 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """returns a new concatenated matrix"""
-    concat = mat1[:]
-    #mat1c = mat1[:]
+    concat = []
     matrix = []
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        for item in mat1:
+            concat.append(item[:])
         for row in mat2:
-            concat.append(row)
+            concat.append(row[:])
         return concat
     if axis == 1 and len(mat1) == len(mat2):
         for i in range(len(mat1)):
@@ -17,6 +18,5 @@ def cat_matrices2D(mat1, mat2, axis=0):
             for j in range(len(mat2[0])):
                 rowx.append(mat2[i][j])
             matrix.append(rowx)
-        return matrix          
-        #return [[mat1c[i].append(mat2[i][j]) for j in range(len(mat2[0]))] for i in range(len(mat2))]
+        return matrix
     return None
