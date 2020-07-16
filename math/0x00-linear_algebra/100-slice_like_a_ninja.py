1f4b1e9d415aba5cadbc36ad2000567e8893358f
mlgomez0 <mlgomez230@hotmail.com>
2020-07-16 02:09:05 +0000

    fix code

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -3,5 +3,17 @@
 
 
 def np_slice(matrix, axes={}):
-	    """returns new matrix"""
-    return (np.dot(mat1, mat2))
+    """returns new matrix"""
+    len_dic = len(axes)
+    args = []
+    list_axes = []
+    max_val = max(axes.keys())
+    for j in range(max_val + 1):
+        list_axes.append(j)
+    for i in list_axes:
+        if i in axes.keys():
+            sl = slice(*axes[i])
+        else:
+            sl = slice(None)
+        args.append(sl)
+    return matrix[tuple(args)]
