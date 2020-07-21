fc5b3dd7eaba2c3db67e37ed987be22db3367a83
mlgomez0 <mlgomez230@hotmail.com>
2020-07-21 23:55:25 +0000

    create files

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""calculates integral of a polinomial"""
+
+def poly_integral(poly, C=0):
+    """returns a list with coefficients"""
+    inte = []
+    i = 0
+    if type(poly) != list or len(poly) == 0 or poly[0] == 0 or poly[len(poly) - 1] == 0 or type(C) != int:
+        return None
+    if len(poly) == 1:
+        return poly
+    inte.append(C)
+    while i < len(poly):
+
+        if poly[i]%(i + 1) == 0:
+            inte.append(int(poly[i]/(i + 1)))
+        else:
+            inte.append(poly[i]/(i + 1))
+        i += 1
+    return inte
--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,10 +2,13 @@
 """function calculates the summation of i**2 starting from 1"""
 
 def summation_i_squared(n):
-    if type(n) != int or n < 0:
+    """returns the summation"""
+    if type(n) != int:
         return None
     if n == 0:
         return 0
+    if n < 0:
+        n = n * -1
     if n == 1:
         return 1
     summation = n**2
