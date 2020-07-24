d2b26554d17a12910f05207db08270006c6f06f5
mlgomez0 <mlgomez230@hotmail.com>
2020-07-24 16:03:53 +0000

    fix return len(poly) == 1

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -8,9 +8,10 @@ def poly_integral(poly, C=0):
     i = 0
     if type(poly) != list or len(poly) == 0 or type(C) != int:
         return None
-    if len(poly) == 1:
-        return poly
     inte.append(C)
+    if len(poly) == 1:
+        inte.append(poly[0])
+        return inte
     while i < len(poly):
         if poly[i] % (i + 1) == 0:
             inte.append(int(poly[i]/(i + 1)))
--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -2,5 +2,6 @@
 
 poly_integral = __import__('17-integrate').poly_integral
 
-poly = [5, 3, 0, 1]
+#poly = [5, 3, 0, 1]
+poly = [5]
 print(poly_integral(poly))
