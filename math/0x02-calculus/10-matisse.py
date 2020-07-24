69902501669d7a28d7d793458c16cf25fd277ac2
mlgomez0 <mlgomez230@hotmail.com>
2020-07-24 14:53:17 +0000

    fix pycodestyle

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-
 """calculates the derivative of a polynomial"""
+
 
 def poly_derivative(poly):
     """returns a list with the derivative coefficients"""
     deriv = []
     i = 1
-    if type(poly) != list or len(poly) == 0 or poly[0] == 0 or poly[len(poly) - 1] == 0:
+    if type(poly) != list or len(poly) == 0 or poly[
+            0] == 0 or poly[len(poly) - 1] == 0:
         return None
     if len(poly) == 1:
         return [0]
