41a93a29c7d40a83b2fc68cc7753748f14174690
mlgomez0 <mlgomez230@hotmail.com>
2020-07-29 16:09:16 +0000

    adding lambtha<= 0

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -6,14 +6,14 @@ class Exponential:
     """this class allows exponential distribution"""
     def __init__(self, data=None, lambtha=1.):
         if data is None:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             else:
                 self.lambtha = float(lambtha)
         else:
             if type(data) != list:
                 raise TypeError("data must be a list")
-            elif len(data) < 2:
+            elif len(data) <= 2:
                 raise ValueError("data must contain multiple values")
             else:
                 self.lambtha = 1 / (sum(data) / len(data))
