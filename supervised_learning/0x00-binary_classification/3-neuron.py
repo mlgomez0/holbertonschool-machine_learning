a9eac9edaeb4cd0c6b8fa92a688937d4c9423114
mlgomez0 <mlgomez230@hotmail.com>
2020-07-31 20:14:03 +0000

    create

---
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""class Neuron"""
+import numpy as np
+
+
+class Neuron:
+    """defines single neuron for Binary Classification"""
+    def __init__(self, nx):
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        cost_array = np.multiply(np.log(A), Y) + np.multiply((1 - Y), np.log(1.0000001 - A))
+        cost = -np.sum(cost_array) / len(A[0])
+        return cost
+
