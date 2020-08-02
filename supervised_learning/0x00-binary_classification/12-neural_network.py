9ba25c37fe6b4cb1abd8d4336fe736495e959f94
mlgomez0 <mlgomez230@hotmail.com>
2020-08-02 00:54:34 +0000

    create

---
+++ supervised_learning/0x00-binary_classification/12-neural_network.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""class NeuralNetwork with one hidden layer"""
+import numpy as np
+
+class NeuralNetwork:
+    """defines single NeuralNetwork for Binary Classification"""
+    def __init__(self, nx, nodes):
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 =  np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        return (self.__A1, self.__A2)
+
+    def cost(self, Y, A):
+        cost_array = (np.log(A) * Y) + ((1 - Y) * np.log(1.0000001 - A))
+        cost = -np.sum(cost_array) / len(A[0])
+        return cost
+
+    def evaluate(self, X, Y):
+        self.forward_prop(X)
+        cost_r = self.cost(Y, self.__A2)
+        return (np.where(self.__A2 >= 0.5, 1, 0), cost_r)
