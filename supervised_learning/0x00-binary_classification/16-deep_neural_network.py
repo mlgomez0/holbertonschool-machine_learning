8ba3c4f206d734b411c75116acb1a850dc225a78
mlgomez0 <mlgomez230@hotmail.com>
2020-08-03 00:57:07 +0000

    create

---
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""class DeepNeuralNetwork with multiple hidden layer"""
+import numpy as np
+
+class DeepNeuralNetwork:
+    """defines deep Neural Network for Binary Classification"""
+    def __init__(self, nx, layers):
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list:
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        num_layer = 1
+        layer_size = nx
+        for i in layers:
+            if type(i) != int or i < 0:
+                raise TypeError("layers must be a list of positive integers")
+            w = "W" + str(num_layer)
+            b = "b" + str(num_layer)
+            self.weights[w] = np.random.randn(i, layer_size) * np.sqrt(2/layer_size)
+            self.weights[b] = np.zeros((i, 1))
+            num_layer += 1
+            layer_size = i
