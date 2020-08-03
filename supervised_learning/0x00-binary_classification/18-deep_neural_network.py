50221bd8b68f563d8186b620356e32365890d387
mlgomez0 <mlgomez230@hotmail.com>
2020-08-03 01:36:31 +0000

    create

---
+++ supervised_learning/0x00-binary_classification/18-deep_neural_network.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""class DeepNeuralNetwork with multiple hidden layer"""
+import numpy as np
+
+class DeepNeuralNetwork:
+    """defines deep Neural Network for Binary Classification"""
+    def __init__(self, nx, layers):
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        num_layer = 1
+        layer_size = nx
+        for i in layers:
+            if type(i) != int or i < 0:
+                raise TypeError("layers must be a list of positive integers")
+            w = "W" + str(num_layer)
+            b = "b" + str(num_layer)
+            self.__weights[w] = np.random.randn(i, layer_size) * np.sqrt(2/layer_size)
+            self.__weights[b] = np.zeros((i, 1))
+            num_layer += 1
+            layer_size = i
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    def forward_prop(self, X):
+        self.__cache["A0"] = X
+        for i in range(1, self.__L + 1):
+            w = "W" + str(i)
+            b = "b" + str(i)
+            a = "A" + str(i - 1)
+            Z = np.matmul(self.__weights[w], self.__cache[a]) + self.__weights[b]
+            a_new = "A" + str(i)
+            self.__cache[a_new] = 1 / (1 + np.exp(-Z))
+        l = "A" + str(self.__L)
+        return (self.__cache[l], self.__cache)
+
