bb032319e9f8746647cd561d2a83305ef3e987d3
mlgomez0 <mlgomez230@hotmail.com>
2020-08-04 21:31:13 +0000

    fix back pycodestyle

--- supervised_learning/0x00-binary_classification/21-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/21-deep_neural_network.py
@@ -68,35 +68,16 @@ class DeepNeuralNetwork:
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """performs gradient descent"""
-        Af = cache["A" + str(self.__L)]
-        dzf = Af - Y
-        dwf = (1 / len(Y[0])) * np.matmul(dzf, Af.T)
-        dbf = (1 / len(Y[0])) * np.sum(dzf, axis=1, keepdims=True)
-        w = "W" + str(self.__L)
-        b = "b" + str(self.__L)
-        self.__weights[w] = self.__weights[w] - alpha * dwf
-        self.__weights[b] = self.__weights[b] - alpha * dbf
-        for i in range(self.__L - 1, 0, -1):
-            W_prev = self.__weights["W" + str(i + 1)]
-            dzf = np.matmul(W_prev.T, dzf) * (cache["A" + str(i)] * (1 - cache["A" + str(i)]))
-            dwf = (1 / len(Y[0])) * np.matmul(dzf, cache["A" + str(i - 1)].T)
-            dbf = (1 / len(Y[0])) * np.sum(dzf, axis=1, keepdims=True)
-            w = "W" + str(i)
-            b = "b" + str(i)
-            self.__weights[w] = self.__weights[w] - alpha * dwf
-            self.__weights[b] = self.__weights[b] - alpha * dbf
-        """
-        da = -(Y/cache["A" + str(self.__L)]) + ((
-               1 - Y)/(1 - cache["A" + str(self.__L)]))
+        da = -Y/cache["A" + str(self.__L)] + (
+               1 - Y)/(1 - cache["A" + str(self.__L)])
         for i in range(self.__L, 0, -1):
             Al = "A" + str(i)
             dz = da * (cache[Al] * (1 - cache[Al]))
-            Al_ = "A" + str(i - 1)
-            dw = (1 / len(Y[0])) * np.matmul(dz, cache[Al_].T)
-            db = (1 / len(Y[0])) * np.sum(dz, axis=1, keepdims=True)
+            Aln = "A" + str(i - 1)
+            dw = 1 / len(Y[0]) * np.matmul(dz, cache[Aln].T)
+            db = 1 / len(Y[0]) * np.sum(dz, axis=1, keepdims=True)
             w = "W" + str(i)
             b = "b" + str(i)
             self.__weights[w] = self.__weights[w] - alpha * dw
             self.__weights[b] = self.__weights[b] - alpha * db
             da = np.matmul(self.__weights["W" + str(i)].T, dz)
-        """
