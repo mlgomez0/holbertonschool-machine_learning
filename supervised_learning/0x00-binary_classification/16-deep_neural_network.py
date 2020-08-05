1b6a0320264e7a88d641d2e3d0b2b8d53170c11c
mlgomez0 <mlgomez230@hotmail.com>
2020-08-05 16:30:09 +0000

    add len(list)

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -10,7 +10,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) != list or len(list) == 0:
+        if type(layers) != list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
