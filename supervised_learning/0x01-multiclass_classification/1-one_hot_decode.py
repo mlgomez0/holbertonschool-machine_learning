f2c59da9762354ca122c52a22911e9ee5f31f068
mlgomez0 <mlgomez230@hotmail.com>
2020-08-11 15:10:09 +0000

    added type check

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -10,4 +10,6 @@ def one_hot_decode(one_hot):
     """returns vector of labels"""
     if type(one_hot) is not np.ndarray or len(one_hot) == 0:
         return None
+    if len(one_hot.shape) != 2:
+        return None
     return np.argmax(one_hot, axis=0)
