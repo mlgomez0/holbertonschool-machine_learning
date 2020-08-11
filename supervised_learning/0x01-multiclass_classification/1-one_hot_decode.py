86b2e6ad1aea3a2d609a74feec9203dc53969362
mlgomez0 <mlgomez230@hotmail.com>
2020-08-11 14:50:00 +0000

    added one hot check

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -10,4 +10,6 @@ def one_hot_decode(one_hot):
     """returns vector of labels"""
     if len(one_hot) == 0:
         return None
+    if np.sum((np.sum(one_hot, axis=0))) != one_hot.shape[1]:
+        return None
     return np.argmax(one_hot, axis=0)
