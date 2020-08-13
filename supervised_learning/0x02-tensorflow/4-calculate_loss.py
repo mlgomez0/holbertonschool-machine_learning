896954b54a21962988d93c0df1fd8f0300236609
mlgomez0 <mlgomez230@hotmail.com>
2020-08-13 20:44:37 +0000

    create

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""Performs accuracy"""
+"""calculates loss"""
 import tensorflow as tf
-create_layer = __import__('1-create_layer').create_layer
 
-def calculate_accuracy(y, y_pred):
-    """tensor with accuracy"""
-    Ac = tf.losses.softmax_cross_entropy(y, y_pred)
-    return Ac
+def calculate_loss(y, y_pred):
+    """tensor with loss"""
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+    return loss
