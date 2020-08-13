e567ea47657a8b4be3b873d1acf15470b46d4385
mlgomez0 <mlgomez230@hotmail.com>
2020-08-13 21:02:37 +0000

    create file

---
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""creates training operation"""
+import tensorflow as tf
+
+def create_train_op(loss, alpha):
+    """operations that trains a model"""
+    optimizer = tf.train.GradientDescentOptimizer(alpha)
+    train = optimizer.minimize(loss)
+    return train
