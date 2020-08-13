24d932b62abd7002a47d7881bc9c0e1b8b1a6ab5
mlgomez0 <mlgomez230@hotmail.com>
2020-08-13 18:08:54 +0000

    create file

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Creates a layer"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """returns tensor ourput layer"""
+    layer = tf.layers.Dense(units=n, activation=activation, bias_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"), name="layer")
+    y = layer(prev)
+    return (y)
