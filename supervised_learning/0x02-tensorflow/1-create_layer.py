2dad649d918acb64b9e27eaea3183b37d364edb5
mlgomez0 <mlgomez230@hotmail.com>
2020-08-13 19:05:04 +0000

    correct weights initializer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -5,6 +5,6 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation):
     """returns tensor ourput layer"""
-    layer = tf.layers.Dense(units=n, activation=activation, bias_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"), name="layer")
+    layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"), name="layer")
     y = layer(prev)
     return (y)
