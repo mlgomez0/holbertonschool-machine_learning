71e1964f759a874ca050eb5a857782db92ad32a1
mlgomez0 <mlgomez230@hotmail.com>
2020-08-19 17:18:40 +0000

    create files

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""returns moving averages"""
+"""returns moving averages with bias correction"""
 import numpy as np
 
 def moving_average(data, beta):
     vp = 0
     weighted = []
     for i in range(len(data)):
-        v = ((vp * beta + (1 - beta) * data[i]))
+        v = (vp * beta + (1 - beta) * data[i])
         weighted.append(v / (1 - beta**(i + 1)))
         vp = v
     return weighted
