c4b4ba6db21721c5b7b10b42327a9302707bbc34
mlgomez0 <mlgomez230@hotmail.com>
2020-08-20 18:33:18 +0000

    fix pycodestyle

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -2,11 +2,12 @@
 """creates training operation
    using gradient descent with RMSProp
 """
-import numpy as np
 import tensorflow as tf
+
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """returns tensor with RMSProp"""
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                          decay=beta2, epsilon=epsilon)
     train = optimizer.minimize(loss)
     return train
