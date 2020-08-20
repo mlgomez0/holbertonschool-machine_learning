c167da8d915b85f6ca58d43a658768cfe27f5617
mlgomez0 <mlgomez230@hotmail.com>
2020-08-20 20:53:58 +0000

    fix pycodestyle

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -61,15 +61,9 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """"returns learning rate decay operation"""
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
-        glo_step = sess.run(global_step)
-        if glo_step % decay_rate == 0:
-            alpha = tf.train.inverse_time_decay(
-                alpha, global_step, decay_step, decay_rate, staircase=True)
-            return alpha
-        else:
-            return alpha
+    return tf.train.inverse_time_decay(alpha, global_step,
+                                       decay_step, decay_rate,
+                                       staircase=True)
 
 
 def shuffle_data(X, Y):
