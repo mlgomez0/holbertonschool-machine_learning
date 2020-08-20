c6e975635559ff7c4353dd558479303c2bfd2c3e
mlgomez0 <mlgomez230@hotmail.com>
2020-08-20 18:18:27 +0000

    fix pycodestyle

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-
-import numpy as np
+"""perform a mini-batch train"""
 import tensorflow as tf
-
-
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5, load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
-       
+
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
+                     epochs=5, load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+    """trains performing minibatch gradient descent"""
     with tf.Session() as sess:
         load_p = load_path + ".meta"
         saver_n = tf.train.import_meta_graph(load_p)
@@ -26,28 +26,27 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5
             flag = False
 
         for i in range(epochs + 1):
-            cost_t, acc_t = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
-            cost_v, acc_v = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+            cos_t, acc_t = sess.run([loss, accuracy], {x: X_train, y: Y_train})
+            cos_v, acc_v = sess.run([loss, accuracy], {x: X_valid, y: Y_valid})
             print("After {} epochs:".format(i))
-            print("\tTraining Cost: {}".format(cost_t))
+            print("\tTraining Cost: {}".format(cos_t))
             print("\tTraining Accuracy: {}".format(acc_t))
-            print("\tValidation Cost: {}".format(cost_v))
+            print("\tValidation Cost: {}".format(cos_v))
             print("\tValidation Accuracy: {}".format(acc_v))
             if i < epochs:
                 x_train_s, y_train_s = shuffle_data(X_train, Y_train)
                 for j in range(step_next):
                     start = j * batch_size
-                    if j == step_next - 1 and flag == True:
+                    if j == step_next - 1 and flag is True:
                         end = X_train.shape[0]
                     else:
                         end = j * batch_size + batch_size
-                    batch_X = x_train_s[start:end]
-                    batch_Y = y_train_s[start:end]
-                    sess.run([train_op], feed_dict={x: batch_X, y: batch_Y})         
+                    b_X = x_train_s[start:end]
+                    b_Y = y_train_s[start:end]
+                    sess.run([train_op], {x: b_X, y: b_Y})
                     if (j + 1) % 100 == 0 and j != 0:
-                        cost_b, acc_b = sess.run([loss, accuracy], feed_dict={x: batch_X, y: batch_Y})
+                        c_b, a_b = sess.run([loss, accuracy], {x: b_X, y: b_Y})
                         print("\tStep {}".format(j + 1))
-                        print("\t\tCost: {}".format(cost_b))
-                        print("\t\tAccuracy: {}".format(acc_b))
-                    
-        return saver_n.save(sess, save_path) 
+                        print("\t\tCost: {}".format(c_b))
+                        print("\t\tAccuracy: {}".format(a_b))
+        return saver_n.save(sess, save_path)
