e379b8714400648ad4fd53231d8f0e04492aa9f0
mlgomez0 <mlgomez230@hotmail.com>
2020-08-20 18:39:42 +0000

    fix pycodestyle

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -7,8 +7,4 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """"returns updated value for alpha"""
-    if global_step % decay_rate == 0:
-        alpha = alpha / (1 + decay_rate * np.floor(global_step / decay_step))
-        return alpha
-    else:
-        return alpha
+    return alpha / (1 + decay_rate * np.floor(global_step / decay_step))
