81f6c2ba727a5d197957a5646a51995c2afd94c8
mlgomez0 <mlgomez230@hotmail.com>
2020-08-20 18:53:50 +0000

    fix pycodestyle

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -2,6 +2,7 @@
 """creates batch normalization layer"""
 import tensorflow as tf
 
+
 def create_batch_norm_layer(prev, n, activation):
     """tensor activated output of the layer"""
 
@@ -12,5 +13,6 @@ def create_batch_norm_layer(prev, n, activation):
     mean, variance = tf.nn.moments(y, [0])
     gamma = tf.Variable(tf.constant(1.0, shape=[n]), trainable=True)
     beta = tf.Variable(tf.constant(0.0, shape=[n]), trainable=True)
-    y_norm = tf.nn.batch_normalization(y, mean, variance, offset=beta, scale=gamma, variance_epsilon=1e-8)
+    y_norm = tf.nn.batch_normalization(y, mean, variance, offset=beta,
+                                       scale=gamma, variance_epsilon=1e-8)
     return (activation(y_norm))
