b5a17bac35660e2c57839c16b6a7324537bc3334
mlgomez0 <mlgomez230@hotmail.com>
2020-08-27 14:20:43 +0000

    change loop for

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -10,6 +10,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     sum_norms = 0
     for i in range(1, L + 1):
         w = "W" + str(i)
-        sum_norms = sum_norms + (np.linalg.norm(w))
+        sum_norms = sum_norms + (np.linalg.norm(weights[w]))
 
     return cost + (sum_norms * (lambtha / (2 * m)))
