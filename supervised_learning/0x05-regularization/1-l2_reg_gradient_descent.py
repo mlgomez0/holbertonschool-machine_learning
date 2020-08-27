06b84a18fb375ca7e3c59bdebff195673a8b4056
mlgomez0 <mlgomez230@hotmail.com>
2020-08-27 15:14:11 +0000

    change formula, don't apply to b

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -10,12 +10,10 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     dz = cache["A" + str(L)] - Y
     for i in range(L, 0, -1):
         A = cache["A" + str(i - 1)]
-        dw = (1 / len(Y[0])) * np.matmul(dz, A.T)
-        db = (1 / len(Y[0])) * np.sum(dz, axis=1, keepdims=True)
         w = "W" + str(i)
         b = "b" + str(i)
-        weights[w] = weights[w] - (
-            alpha * lambtha) / len(Y[0]) * weights[w] - alpha * dw
-        weights[b] = weights[b] - (
-            alpha * lambtha) / len(Y[0]) * weights[b] - alpha * db
+        dw = (1 / len(Y[0])) * np.matmul(dz, A.T) + (lambtha * weights[w]) / len(Y[0])
+        db = (1 / len(Y[0])) * np.sum(dz, axis=1, keepdims=True)
+        weights[w] = weights[w] - alpha * dw
+        weights[b] = weights[b] - alpha * db
         dz = np.matmul(weights["W" + str(i)].T, dz) * (1 - A * A)
