dc20192ff0a0f536068497cf11e349899617a39c
mlgomez0 <mlgomez230@hotmail.com>
2020-09-03 17:40:43 +0000

    create files

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -10,12 +10,13 @@ def convolve_grayscale_same(images, kernel):
     hk = kernel.shape[0]
     wk = kernel.shape[1]
     pad_h = int((hk - 1) / 2)
+    pad_w = int((wk - 1) / 2)
     m = images.shape[0]
     convoluted = np.zeros((m, out_h, out_w))
-    img = np.pad(images, ((0,0), (pad_h,pad_h), (pad_h,pad_h)), 'constant')
+    img = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), 'constant')
     for h in range(out_h):
         for w in range(out_w):
-            matrix = img[:, h : h + hk, w : w + wk]
+            matrix = img[:, h: h + hk, w: w + wk]
             v = np.sum(matrix * kernel, axis=1).sum(axis=1)
             convoluted[:, h, w] = v
     return(convoluted)
