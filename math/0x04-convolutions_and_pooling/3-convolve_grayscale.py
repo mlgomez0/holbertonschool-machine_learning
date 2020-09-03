fd8c0c103a6f92e97a738ddf32a0799e713430e3
mlgomez0 <mlgomez230@hotmail.com>
2020-09-03 18:44:40 +0000

    create files

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """performs convolution of gray images using custom padding"""
 import numpy as np
-from math import ceil, floor
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
@@ -22,12 +21,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     else:
         pad0 = padding[0]
         pad1 = padding[1]
-    out_h = int(floor(float(
-                hm + 2 * pad0 - hk) / float(
-                st0))) + 1
-    out_w = int(floor(float(
-                wm + 2 * pad1 - wk) / float(
-                st1))) + 1
+    out_h = int((hm + 2 * pad0 - hk) / st0) + 1
+    out_w = int((wm + 2 * pad1 - wk) / st1) + 1
     convoluted = np.zeros((m, out_h, out_w))
     img = np.pad(images, ((0, 0), (pad0, pad0), (pad1, pad1)), 'constant')
     for h in range(out_h):
