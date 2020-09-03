d07461d5766446c81513e8cba1a9baee8ba17376
mlgomez0 <mlgomez230@hotmail.com>
2020-09-03 17:36:38 +0000

    create files

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -10,12 +10,12 @@ def convolve_grayscale_valid(images, kernel):
     wm = images.shape[2]
     hk = kernel.shape[0]
     wk = kernel.shape[1]
-    ch = hm - hk + 1 
+    ch = hm - hk + 1
     cw = wm - wk + 1
     convoluted = np.zeros((m, ch, cw))
     for h in range(ch):
         for w in range(cw):
-            matrix = images[:, h : h + hk, w : w + wk]
+            matrix = images[:, h: h + hk, w: w + wk]
             v = np.sum(matrix * kernel, axis=1).sum(axis=1)
             convoluted[:, h, w] = v
     return(convoluted)
