49603d9c436afd2975e41ecc6d55675cfb1f3634
mlgomez0 <mlgomez230@hotmail.com>
2020-09-03 15:54:29 +0000

    create files

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-"""performs convolution of gray images using padding"""
+"""performs convolution of gray images using custom padding"""
 import numpy as np
 
 
-def convolve_grayscale_same(images, kernel):
+def convolve_grayscale_padding(images, kernel, padding):
     """returns convolved matrix"""
-    out_h = images.shape[1]
-    out_w = images.shape[2]
+      
     hk = kernel.shape[0]
     wk = kernel.shape[1]
-    pad_h = int((hk - 1) / 2)
     m = images.shape[0]
+    out_h = images.shape[1] + 2 * padding[0]  - hk + 1
+    out_w = images.shape[2] + 2 * padding[1] - wk + 1
     convoluted = np.zeros((m, out_h, out_w))
-    img = np.pad(images, ((0,0), (pad_h,pad_h), (pad_h,pad_h)), 'constant')
+    img = np.pad(images, ((0,0), (padding[0],padding[0]), (padding[1],padding[1])), 'constant')
     for h in range(out_h):
         for w in range(out_w):
             matrix = img[:, h : h + hk, w : w + wk]
---
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""performs convolution of gray images using custom padding"""
+import numpy as np
+from math import ceil, floor
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """returns convolved matrix"""
+    hk = kernel.shape[0]
+    wk = kernel.shape[1]
+    m = images.shape[0]
+    if padding == "valid":
+        pad0 = 0
+        pad1 = 0
+    elif padding == "same":
+        pad0 = int((hk - 1) / 2)
+        pad1 = int((wk - 1) / 2)
+    else:
+        pad0 = padding[0]
+        pad1 = padding[1]
+    out_h = int(floor(float(images.shape[1] + 2 * pad0  - hk) / float(stride[0]))) + 1
+    out_w = int(floor(float(images.shape[2] + 2 * pad1 - wk) / float(stride[1]))) + 1
+    convoluted = np.zeros((m, out_h, out_w))
+    img = np.pad(images, ((0,0), (pad0,pad0), (pad1,pad1)), 'constant')
+    for h in range(out_h):
+        for w in range(out_w):
+            matrix = img[:, h * stride[0] : h * stride[0] + hk, w * stride[1] : w * stride[1] + wk]
+            v = np.sum(matrix * kernel, axis=1).sum(axis=1)
+            convoluted[:, h, w] = v
+    return(convoluted)
