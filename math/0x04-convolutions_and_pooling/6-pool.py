670b21e0354bc98aef81e2fa61aeadb2c1af140c
mlgomez0 <mlgomez230@hotmail.com>
2020-09-03 19:20:49 +0000

    create files

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 """performs convolution images using pooling"""
 import numpy as np
-from math import ceil, floor
 
 
 def pool(images, kernel_shape, stride, mode='max'):
     """returns convolved matrix"""
     hk = kernel_shape[0]
     wk = kernel_shape[1]
-    image_h = images.shape[1]
-    image_w = images.shape[2]
     nc = images.shape[3]
     m = images.shape[0]
-    out_h = int(floor(float(image_h - hk) / float(stride[0]))) + 1
-    out_w = int(floor(float(image_w - wk) / float(stride[1]))) + 1
+    hm = images.shape[1]
+    wm = images.shape[2]
+    st1 = stride[1]
+    st0 = stride[0]
+    out_h = int((hm - hk) / st0) + 1
+    out_w = int((wm - wk) / st1) + 1
     pooled = np.zeros((m, out_h, out_w, nc))
     img = images.copy()
     for h in range(out_h):
         for w in range(out_w):
-            matrix = img[:, h * stride[0] : h * stride[0] + hk, w * stride[1] : w * stride[1] + wk,:]
+            matrix = img[:, h * st0: h * st0 + hk, w * st1: w * st1 + wk, :]
             if mode == "max":
-                v = np.max(matrix, axis=(1,2))
+                v = np.max(matrix, axis=(1, 2))
             else:
-                v = np.average(matrix, axis=(1,2))
-            pooled[:, h, w,:] = v
+                v = np.average(matrix, axis=(1, 2))
+            pooled[:, h, w, :] = v
     return(pooled)
