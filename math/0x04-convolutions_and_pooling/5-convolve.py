5355c680ffe46e32f9720594acd2392de1b76286
mlgomez0 <mlgomez230@hotmail.com>
2020-09-03 19:11:34 +0000

    create files

--- math/0x04-convolutions_and_pooling/5-convolve.py
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """performs convolution of images with channels"""
 import numpy as np
-from math import ceil, floor
 
 
 def convolve(images, kernels, padding='same', stride=(1, 1)):
@@ -10,23 +9,29 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     wk = kernels.shape[1]
     nc = kernels.shape[3]
     m = images.shape[0]
+    hm = images.shape[1]
+    wm = images.shape[2]
+    st1 = stride[1]
+    st0 = stride[0]
     if padding == "valid":
         pad0 = 0
         pad1 = 0
     elif padding == "same":
-        pad0 = int((hk - 1) / 2)
-        pad1 = int((wk - 1) / 2)
+        pad0 = int(((hm - 1) * st0 + hk - hm) / 2) + 1
+        pad1 = int(((wm - 1) * st1 + wk - wm) / 2) + 1
     else:
         pad0 = padding[0]
         pad1 = padding[1]
-    out_h = int(floor(float(images.shape[1] + 2 * pad0  - hk) / float(stride[0]))) + 1
-    out_w = int(floor(float(images.shape[2] + 2 * pad1 - wk) / float(stride[1]))) + 1
+    out_h = int((hm + 2 * pad0 - hk) / st0) + 1
+    out_w = int((wm + 2 * pad1 - wk) / st1) + 1
     convoluted = np.zeros((m, out_h, out_w, nc))
-    img = np.pad(images, ((0,0), (pad0,pad0), (pad1,pad1), (0,0)), 'constant')
+    img = np.pad(images, ((0, 0), (pad0, pad0), (
+                           pad1, pad1), (0, 0)), 'constant')
     for c in range(nc):
         for h in range(out_h):
             for w in range(out_w):
-                matrix = img[:, h * stride[0] : h * stride[0] + hk, w * stride[1] : w * stride[1] + wk, :]
-                v = np.sum(matrix * kernels[..., c], axis=1).sum(axis=1).sum(axis=1)
+                ma = img[:, h * st0: h * st0 + hk, w * st1: w * st1 + wk, :]
+                vm = np.sum(ma * kernels[..., c], axis=1).sum(axis=1)
+                v = vm.sum(axis=1)
                 convoluted[:, h, w, c] = v
     return(convoluted)
