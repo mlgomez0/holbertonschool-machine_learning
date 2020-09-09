eba5d37e801106304562e9a82110d08b4b86d97b
mlgomez0 <mlgomez230@hotmail.com>
2020-09-09 17:11:30 +0000

    remove pad and use Dense

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -10,14 +10,13 @@ def lenet5(x, y):
        (with default hyperparameters)
        -a tensor for the loss of the netowrk
        -a tensor for the accuracy of the network"""
-    pad_x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], "CONSTANT")
     w = tf.contrib.layers.variance_scaling_initializer()
     conv1 = tf.layers.Conv2D(
           filters=6,
           kernel_size=5,
           padding="same",
           kernel_initializer=w,
-          activation=tf.nn.relu)(pad_x)
+          activation=tf.nn.relu)(x)
     max_pool1 = tf.layers.MaxPooling2D(
           pool_size=2,
           strides=2)(conv1)
@@ -31,11 +30,11 @@ def lenet5(x, y):
           pool_size=2,
           strides=2)(conv2)
     flat_layer = tf.layers.Flatten()(max_pool2)
-    full_layer1 = tf.layers.dense(flat_layer, units=120, activation=tf.nn.relu,
-                                  kernel_initializer=w)
-    full_layer2 = tf.layers.dense(full_layer1, units=84, activation=tf.nn.relu,
-                                  kernel_initializer=w)
-    full_layer3 = tf.layers.dense(full_layer2, units=10, kernel_initializer=w)
+    full_layer1 = tf.layers.Dense(units=120, activation=tf.nn.relu,
+                                  kernel_initializer=w)(flat_layer)
+    full_layer2 = tf.layers.Dense(units=84, activation=tf.nn.relu,
+                                  kernel_initializer=w)(full_layer1)
+    full_layer3 = tf.layers.Dense(units=10, kernel_initializer=w)(full_layer2)
     y_pred = tf.nn.softmax(full_layer3)
 
     equal = tf.equal(tf.argmax(full_layer3, 1), tf.argmax(y, 1))
