8bc1f5636f7c7d4fc5d8ce02b3168738c5f5c2f4
mlgomez0 <mlgomez230@hotmail.com>
2020-09-25 18:37:26 +0000

    create files

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -6,24 +6,23 @@
 import tensorflow.keras as K
 
 
-def preprocess_data1(X, Y):
+def preprocess_data(X, Y):
     """preprocess data so it is accepted by ResNet50 model"""
     x_test = K.applications.resnet50.preprocess_input(X)
     y_test = K.utils.to_categorical(Y)
     return x_test, y_test
 
 
-def resize_images1(X):
-    """resize data so it is accepted by ResNet50 model"""
-    return K.backend.resize_images(X, 7, 7,
-                                   data_format="channels_last",
-                                   interpolation='bilinear')
-
-
 if __name__ == "__main__":
+
+    def resize_images1(X):
+        """resize data so it is accepted by ResNet50 model"""
+        return K.backend.resize_images(X, 7, 7,
+                                       data_format="channels_last",
+                                       interpolation='bilinear')
     (X_train, Y_train), (
      X_valid, Y_valid) = K.datasets.cifar10.load_data()
-    optimizer = K.optimizers.Adam(lr=0.001)
+    optimizer = K.optimizers.Adam(lr=0.00001)
     ResNet50_model = K.applications.ResNet50(weights='imagenet',
                                              include_top=False,
                                              input_shape=(224, 224, 3))
@@ -32,16 +31,21 @@ if __name__ == "__main__":
     lambda_1 = K.layers.Lambda(resize_images1)(input)
     x = ResNet50_model(lambda_1, training=False)
     x = K.layers.Flatten()(x)
-    x = K.layers.Dense(1000, activation='relu')(x)
-    x = K.layers.Dropout(0.3)(x)
+    x = K.layers.BatchNormalization()(x)
+    x = K.layers.Dense(128, activation='relu')(x)
+    x = K.layers.Dropout(0.5)(x)
+    x = K.layers.BatchNormalization()(x)
+    x = K.layers.Dense(64, activation='relu')(x)
+    x = K.layers.Dropout(0.5)(x)
+    x = K.layers.BatchNormalization()(x)
     x = K.layers.Dense(10, activation='softmax')(x)
     model = K.Model(input, x)
     model.compile(optimizer=optimizer,
                   loss='categorical_crossentropy', metrics=['accuracy'])
-    epochs = 5
+    epochs = 20
     batch_size = 256
-    xt_prep, yt_prep = preprocess_data1(X_train, Y_train)
-    xv_prep, yv_prep = preprocess_data1(X_valid, Y_valid)
+    xt_prep, yt_prep = preprocess_data(X_train, Y_train)
+    xv_prep, yv_prep = preprocess_data(X_valid, Y_valid)
     my_callbacks = [K.callbacks.ModelCheckpoint(
                     filepath='cifar10.h5', save_best_only=True)]
     model.fit(xt_prep, yt_prep,
