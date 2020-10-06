492ffe5c334f57ea65ce2cc8fcdf9f14528653cc
mlgomez0 <mlgomez230@hotmail.com>
2020-10-06 19:10:47 +0000

    adding detect method

--- supervised_learning/0x0B-face_verification/align.py
+++ supervised_learning/0x0B-face_verification/align.py
@@ -9,3 +9,13 @@ class FaceAlign():
         """initializes a FaceAlign instance"""
         self.detector = dlib.get_frontal_face_detector()
         self.shape_predictor = dlib.shape_predictor(shape_predictor_path)
+
+    def detect(self, image):
+        """Returns: a dlib.rectangle containing
+           the boundary box for the face in the
+           image, or None on failure"""
+        boxes = self.detector(image)
+        if len(boxes) >= 1:
+            return max(boxes, key=lambda rect: rect.width() * rect.height())
+        else:
+            return dlib.rectangle(0, 0, image.shape[1], image.shape[0]) 
